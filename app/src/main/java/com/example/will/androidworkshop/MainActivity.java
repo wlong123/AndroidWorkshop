3125b35a150cb1cf05b0820fc9773bb2c7230e92
pdarbha <32006167+pdarbha@users.noreply.github.com>
2018-11-11 23:30:36 -0500

    made it a change the color app

--- app/src/main/java/com/example/will/androidworkshop/MainActivity.java
+++ app/src/main/java/com/example/will/androidworkshop/MainActivity.java
@@ -1,20 +1,24 @@
 package com.example.will.androidworkshop;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import java.lang.Math;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean game = false;
-    private int score = 0;
+    private int color = 0;
 
 
     private Button start;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private Button button2;
     private Button button3;
     private Button button4;
+    private ConstraintLayout background;
 
 
     @Override
@@ -32,29 +37,71 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        start = (Button) findViewById(R.id.start_button);
-        quit = (Button) findViewById(R.id.stop_button);
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
-        button4 = (Button) findViewById(R.id.button4);
 
+        background = (ConstraintLayout) findViewById(R.id.background);
+        start = (Button) findViewById(R.id.start_button);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                game = true;
-                start.setText(R.string.Started);
-//                playGame();
+                int red = (int) (Math.random()*256);
+                int blue = (int) (Math.random()*256);
+                int green = (int) (Math.random()*256);
+                background.setBackgroundColor(Color.rgb(red, green, blue));
             }
         });
-
+        quit = (Button) findViewById(R.id.stop_button);
         quit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                game = false;
-                start.setText(R.string.Start);
+                background.setBackgroundColor(Color.rgb(255,255,255));
             }
         });
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                background.setBackgroundColor(Color.rgb(240,180,0));
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                background.setBackgroundColor(Color.rgb(50,50,240));
+            }
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                background.setBackgroundColor(Color.rgb(153,50,204));
+            }
+        });
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                background.setBackgroundColor(Color.rgb(240,240,0));
+            }
+        });
+
+
+//        start.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                game = true;
+//                start.setText(R.string.Started);
+//            }
+//        });
+//
+//        quit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                game = false;
+//                start.setText(R.string.Start);
+//            }
+//        });
+//        playGame();
     }
 
     public void playGame() {
@@ -79,19 +126,9 @@ public class MainActivity extends AppCompatActivity {
 //    @Override
 //    protected void onStart() {
 //        super.onStart();
-//        while (!game) { }
-//            while (game) {
-//                int nextButton = (int) (Math.random() * 4 + 1);
-//                if (nextButton == 1) {
-//                    button1.setText(R.string.click);
-//                } else if (nextButton == 2) {
-//                    button2.setText(R.string.click);
-//                } else if (nextButton == 3) {
-//                    button3.setText(R.string.click);
-//                } else if (nextButton == 4) {
-//                    button4.setText(R.string.click);
-//                }
-//            }
+//        for(int i=0; i<10; i++){
+//
+//        }
 //    }
 
     @Override
